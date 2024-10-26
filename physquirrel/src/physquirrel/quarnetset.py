397ee24c9740fcb9413029028aa0598f9b77abed
nholtgrefe <136988997+nholtgrefe@users.noreply.github.com>
2024-10-26 21:02:14 +0000

    Update quarnetset.py

--- physquirrel/src/physquirrel/quarnetset.py
+++ physquirrel/src/physquirrel/quarnetset.py
@@ -2,7 +2,8 @@ import networkx as nx
 import itertools, random, math, time, os
 import numpy as np
 from functools import lru_cache
-from python_tsp.exact import solve_tsp_dynamic_programming
+#from python_tsp.exact import solve_tsp_dynamic_programming
+from typing import Dict, List, Optional, Tuple
 
 
 from .utils import CircularOrdering, id_generator, validate
@@ -333,7 +334,7 @@ class DenseQuarnetSet(QuarnetSet):
         elif measure == "QS":
             return 1 - self.distance(quarnetset, triangles=triangles, reticulations=reticulations)        
     
-    def consistency(self, quarnetset, triangles=True, reticulations=True, weighted=True):
+    def consistency(self, quarnetset, triangles=False, reticulations=True, weighted=True):
         """Returns the C-measure of the quarnetset (i.e. ratio of consistent quarnets).  
         Raises an error if the other quarnetsets have a different leafset or are not dense.
         The optional weighted parameter allows for a weighted version of the C-measure. Triangles indicates
@@ -375,7 +376,7 @@ class DenseQuarnetSet(QuarnetSet):
             
         return r1 / r2
     
-    def consistency_prime(self, quarnetset, reference, triangles=True, reticulations=True, weighted=True):
+    def consistency_prime(self, quarnetset, reference, triangles=False, reticulations=True, weighted=True):
         """Returns the C'-measure of the quarnetset w.r.t. the quarnetset reference.  
         If in either of the quarnetsets triangles are collapsed, they will be in both.        
         Raises an error if the other quarnetsets have a different leafset or are not dense.
@@ -421,7 +422,7 @@ class DenseQuarnetSet(QuarnetSet):
         
         return r1 / r2
     
-    def distance(self, quarnetset, triangles=True, reticulations=True, normalize=True):
+    def distance(self, quarnetset, triangles=False, reticulations=True, normalize=True):
         """Returns the S-distance (symmetric difference) w.r.t. another quarnetset. The distance can optionally
         be normalized. Raises an error if the other quarnetset has a different leafset or is not dense.
         Triangles indicates whether triangles should be accounted for, reticulations indicate whether reticulations in four-cycles
@@ -539,7 +540,7 @@ class DenseQuarnetSet(QuarnetSet):
                     distance_matrix[i][j] += delta
                     distance_matrix[j][i] += delta
     
-            permutation, _ = solve_tsp_dynamic_programming(distance_matrix)
+            permutation, _ = _solve_tsp_dynamic_programming(distance_matrix)
             tour = [leaf_order[i] for i in permutation]
             
         else:
@@ -576,7 +577,8 @@ class DenseQuarnetSet(QuarnetSet):
         dense quarnet set. Repeatedly contracts the least supported edge in the
         quartet-joining tree, and then builds the network. If method="best", the
         network with the highest similarity score is returned, if method="first",
-        the last network for which the score did not drop is returned. 
+        the last network for which the score did not drop is returned. If an outgroup is specified
+        the method returns DirectedNetworks rooted at the outgroup.
             include_score: whether to return the similarity measure
             verbose: whether to print intermediate info
             visualize: whether to plot intermediate networks
@@ -632,11 +634,15 @@ class DenseQuarnetSet(QuarnetSet):
         if verbose == True:
             print(f"\rBest network from iteration {best_network_index+1}: score = {scores[best_network_index]}", end='')
         
+        if outgroup:
+            networks = [network.to_directed_network(outgroup) for network in networks]
+            
         if all_networks == False:
             if include_score == False:
                 return networks[best_network_index]
             else:
                 return networks[best_network_index], scores[best_network_index]
+        
         elif all_networks == True:
             paired_lists = list(zip(scores, networks))
             paired_lists.sort(reverse=True, key=lambda x: x[0])
@@ -907,3 +913,125 @@ class DenseQuarnetSet(QuarnetSet):
         raise AttributeError("DenseQuarnetSet is an immutable class.")
    
 ############################################################
+
+
+def _solve_tsp_dynamic_programming(
+    distance_matrix: np.ndarray,
+    maxsize: Optional[int] = None,
+) -> Tuple[List, float]:
+    """
+    Taken from the 'python_tsp' package source code (under the MIT license).
+    
+    Solve TSP to optimality with dynamic programming
+
+    Parameters
+    ----------
+    distance_matrix
+        Distance matrix of shape (n x n) with the (i, j) entry indicating the
+        distance from node i to j. It does not need to be symmetric
+
+    maxsize
+        Parameter passed to ``lru_cache`` decorator. Used to define the maximum
+        size for the recursion tree. Defaults to `None`, which essentially
+        means "take as much space as needed".
+
+    Returns
+    -------
+    permutation
+        A permutation of nodes from 0 to n that produces the least total
+        distance
+
+    distance
+        The total distance the optimal permutation produces
+
+    Notes
+    -----
+    Algorithm: cost of the optimal path
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Consider a TSP instance with 3 nodes: {0, 1, 2}. Let dist(0, {1, 2}) be the
+    distance from 0, visiting all nodes in {1, 2} and going back to 0. This can
+    be computed recursively as:
+
+        dist(0, {1, 2}) = min(
+            c_{0, 1} + dist(1, {2}),
+            c_{0, 2} + dist(2, {1}),
+        )
+
+    wherein c_{0, 1} is the cost from going from 0 to 1 in the distance matrix.
+    The inner dist(1, {2}) is computed as:
+
+        dist(1, {2}) = min(
+            c_{1, 2} + dist(2, {}),
+        )
+
+    and similarly for dist(2, {1}). The stopping point in the recursion is:
+
+        dist(2, {}) = c_{2, 0}.
+
+    This process can be generalized as:
+
+        dist(ni, N) =   min   ( c_{ni, nj} + dist(nj, N - {nj}) )
+                      nj in N
+
+    and
+
+        dist(ni, {}) = c_{ni, 0}
+
+    With starting point as dist(0, {1, 2, ..., tsp_size}). The notation
+    N - {nj} is the difference operator, meaning set N without node nj.
+
+
+    Algorithm: compute the optimal path
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    The previous process returns the distance of the optimal path. To find the
+    actual path, we need to store in a memory the following key/values:
+
+        memo[(ni, N)] = nj_min
+
+    with nj_min the node in N that provided the smallest value of dist(ni, N).
+    Then, the process goes backwards starting from
+    memo[(0, {1, 2, ..., tsp_size})].
+
+    In the previous example, suppose memo[(0, {1, 2})] = 1.
+    Then, look for memo[(1, {2})] = 2.
+    Then, since the next step would be memo[2, {}], stop there. The optimal
+    path would be 0 -> 1 -> 2 -> 0.
+
+    Reference
+    ---------
+    https://en.wikipedia.org/wiki/Held%E2%80%93Karp_algorithm#cite_note-5
+    """
+    # Get initial set {1, 2, ..., tsp_size} as a frozenset because @lru_cache
+    # requires a hashable type
+    N = frozenset(range(1, distance_matrix.shape[0]))
+    memo: Dict[Tuple, int] = {}
+
+    # Step 1: get minimum distance
+    @lru_cache(maxsize=maxsize)
+    def dist(ni: int, N: frozenset) -> float:
+        if not N:
+            return distance_matrix[ni, 0]
+
+        # Store the costs in the form (nj, dist(nj, N))
+        costs = [
+            (nj, distance_matrix[ni, nj] + dist(nj, N.difference({nj})))
+            for nj in N
+        ]
+        nmin, min_cost = min(costs, key=lambda x: x[1])
+        memo[(ni, N)] = nmin
+
+        return min_cost
+
+    best_distance = dist(0, N)
+
+    # Step 2: get path with the minimum distance
+    ni = 0  # start at the origin
+    solution = [0]
+
+    while N:
+        ni = memo[(ni, N)]
+        solution.append(ni)
+        N = N.difference({ni})
+
+    return solution, best_distance
