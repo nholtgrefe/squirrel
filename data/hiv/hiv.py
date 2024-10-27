128378b75cdc03468e4289fced91a02bdefc0817
nholtgrefe <136988997+nholtgrefe@users.noreply.github.com>
2024-10-27 14:14:26 +0000

    Create hiv.py

---
+++ data/hiv/hiv.py
@@ -0,0 +1,8 @@
+from physquirrel import MSA
+
+file = '/path/to/folder/hiv.fasta'
+msa = MSA(file)
+Q = msa.delta_heuristic(lam=0.3)
+
+networks, scores = Q.squirrel(include_score=True, triangles=False, all_networks=True, outgroup='C')
+networks[0].visualize(title="Constructed HIV network")
