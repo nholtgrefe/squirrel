97623840ebbd5daca37ef82264195fa86996e30a
nholtgrefe <136988997+nholtgrefe@users.noreply.github.com>
2024-10-27 14:42:17 +0000

    Update hiv.py

--- data/hiv/hiv.py
+++ data/hiv/hiv.py
@@ -1,8 +1,12 @@
 from physquirrel import MSA
 
+# Load the alignment
 file = '/path/to/folder/hiv.fasta'
 msa = MSA(file)
+
+# Create tf-quarnets
 Q = msa.delta_heuristic(lam=0.3)
 
+# Construct networks
 networks, scores = Q.squirrel(include_score=True, triangles=False, all_networks=True, outgroup='C')
 networks[0].visualize(layout='dot', title="Constructed HIV network")
