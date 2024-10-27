a15671f9fdf36803ad2f1e8e288d723b9f68c722
nholtgrefe <136988997+nholtgrefe@users.noreply.github.com>
2024-10-27 14:42:42 +0000

    Update primates.py

--- data/primates/primates.py
+++ data/primates/primates.py
@@ -1,12 +1,13 @@
 from physquirrel import MSA
 
+# Load the alignment
 file = "/path/to/folder/1730_ALIGNMENT_CONCAT.paup.nex"
 msa = MSA(data_folder + file)
 
-# Make it a dense quarnet set
+# Create tf-quarnets
 Q = msa.delta_heuristic(lam=0.3)
 
-# Construct the network
+# Construct networks
 outgroup = 'Mus_musculus'
 networks, scores = Q.squirrel(include_score=True, all_networks=True, outgroup=outgroup)
 networks[0].visualize(layout="dot",title="Constructed Primate network")
