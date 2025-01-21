1dbaaf6197f013f15b0de731aa2de586cb508e0d
nholtgrefe <136988997+nholtgrefe@users.noreply.github.com>
2025-01-21 17:35:34 +0000

    Update experiment2.py

--- data/simulations/experiment2/experiment2.py
+++ data/simulations/experiment2/experiment2.py
@@ -1,55 +1,55 @@
 import physquirrel as psq
-import os, time, re
+import os, re
     
-network_folder = "/home/nholtgreve/Documents/Projects/Phylogenetics/Level1 heuristic from quarnets/Code/SQuIrReL/data/experiments/generated_networks/"
+network_folder = "/path/to/folder/with/generated_sdnetworks/"
 network_files = os.listdir(network_folder)
 network_files.sort()
 
-sequence_folder = "/home/nholtgreve/Documents/Projects/Phylogenetics/Level1 heuristic from quarnets/Code/SQuIrReL/data/experiments/simulated_alignments/"
+sequence_folder = "/path/to/folder/with/simulated_alignments/"
 sequence_files = os.listdir(sequence_folder)
 sequence_files.sort()
 
-result_file = "/home/nholtgreve/Documents/Projects/Phylogenetics/Level1 heuristic from quarnets/Code/SQuIrReL/data/experiments/result_sequence_experiment.txt"
+result_file = "/path/to/file/with/results/results_experiment2.txt"
 
 with open(result_file, 'a') as f:
-    f.write("file \t delta-QC \t delta-1-QS \t squirrel-QC \t squirrel-1-QS \t ret \t delta-time \t squirrel-time \n")
+    f.write("file \t QC \t QS \t ret \n")
 
 for network_file in network_files:
     match = re.match(r"SDN(\d+)_L(\d+)_R(\d+)", network_file)
     id_string, _, _ = match.groups()
-    
+
+    # Load original SD-network
     N = psq.SemiDirectedNetwork()
     N.load_from_file(network_folder + network_file)
 
+    # Create quarnets of original network
     Q = N.quarnets(triangles=False)
-    
+
+    # Get filenames of corresponding alignments
     sequence_files_id = [f for f in sequence_files if f.startswith('SEQ'+id_string)]
 
     for sequence_file in sequence_files_id:
-                    
+        # Create an MSA object from alignment
         msa = psq.MSA(sequence_folder + sequence_file)
-        
-        time1 = time.perf_counter()
+
+        # Use delta-heuristic to create quarnets
         Q_delta = msa.delta_heuristic()
-        time2 = time.perf_counter()
-                    
+
+        # Use Squirrel to construct network
         N_reconstructed = Q_delta.squirrel()
-        time3 = time.perf_counter()
-        
+
+        # Get quarnets of constructed network
         Q_recon = N_reconstructed.quarnets()
-        
-        score1 = Q.consistency(Q_delta, triangles=False) # QC
-        score2 = 1 - Q.distance(Q_delta, triangles=False) #QS
-        
-        score3 = Q.consistency(Q_recon, triangles=False) # QC
-        score4 = 1 - Q.distance(Q_recon, triangles=False) #QS
-        
-        time_delta = time2 - time1
-        time_squirrel = time3 - time2
-        
+
+        #Compute the two scores
+        score1 = Q.consistency(Q_recon, triangles=False) # QC
+        score2 = 1 - Q.distance(Q_recon, triangles=False) #QS
+
+        # Get number of reticulations of newly constructed network
         ret = len(N_reconstructed.reticulation_nodes())
 
-        info = [score1, score2, score3, score4, ret, time_delta, time_squirrel]
+        # Write results to file
+        info = [score1, score2, ret]
         with open(result_file, 'a') as f:
             f.write(sequence_file + "\t")
             f.write("\t".join(map(str, info)) + "\n")
